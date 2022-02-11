8cfd331efa7ffbb228f7712f7c062bc808adba28
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-10 20:57:39 -0500

    create a reusable Card component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackItem";
+import FeedbackList from "./components/FeeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import Card from "./components/shared/Card";
 
 function App() {
-	const [feedback, setFeedback] = useState();
+	const [feedback, setFeedback] = useState(FeedbackData);
 
 	return (
 		<>
 			<Header />
 			<div className="container">
 				<FeedbackList feedback={feedback} />
+				<Card>Hello Pete</Card>
 			</div>
 		</>
 	);

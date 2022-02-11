d059891d01d70502902d9b996800d6a7047385ef
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-10 19:54:24 -0500

    add styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 function App() {
 	return (
 		<>
-			<Header text="Hello" />
+			<Header />
 			<div>
 				<h1>Hello from the app component</h1>
 			</div>

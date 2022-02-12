983994b74b96f7fada66ad269ac8f396e295391a
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-12 17:54:01 -0500

    create the about page and route to it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import { v4 as uuidv4 } from "uuid";
-import aboutPage from "./pages/aboutPage";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLinks from "./components/AboutIconLinks";
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -25,11 +26,27 @@ function App() {
 	return (
 		<Router>
 			<Header />
+
 			<div className="container">
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-				<Route path="/about" component={aboutPage}>
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}
+					></Route>
+
+					<Route path="/about" element={<AboutPage />} />
+				</Routes>
+				<AboutIconLinks />
 			</div>
 		</Router>
 	);
--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
 
 function aboutPage() {
-  return (
-    <div>aboutPage</div>
-  )
+	return (
+		<Card>
+			<div className="about">About This page </div>;
+			<p>This is a React app to leave feedbacks for our products.</p>
+			<p>
+				<Link to="/">Back To Home</Link>
+			</p>
+		</Card>
+	);
 }
 
-export default aboutPage
+export default aboutPage;
